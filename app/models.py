eab2b0faeae2b348f7b4cc0542007dcb0ba1ccb3
Paula Molina <pau.molina@duocuc.cl>
2025-04-19 21:30:25 -0400

    configure custom user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from app.managers import UserManager
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+    )
+
+    is_administrator = models.BooleanField(default=False)
+    is_partner = models.BooleanField(default=False)
+    description = models.TextField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
--- petti/settings.py
+++ petti/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'petti.urls'
 
+AUTH_USER_MODEL = 'app.User'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',

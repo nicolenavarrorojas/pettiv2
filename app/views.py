1cf917cb882aa7c60217898bc88ae7db7e525cff
Paula Molina <pau.molina@duocuc.cl>
2025-04-19 22:31:37 -0400

    add register view and form

--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.urls import reverse
+from app import forms
 
 def index(request):
     return render(request, 'index.html')
@@ -7,7 +11,31 @@ def login_view(request):
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            user = form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(
+                username=email,
+                password=raw_password
+            )
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+            
+        else:
+            print('form is invalid')
+            print(form.errors)
+        context = {
+            'form': form
+        }
+        return render(request, 'register.html', context)
+    else:
+        raise Exception('Invalid request method')
 
 def services(request):
     return render(request, 'services.html')

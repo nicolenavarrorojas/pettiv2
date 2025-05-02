a25c43274c10ea0977cef579e0aeba89765a1baf
Anth1tes1s <ca.loncuante@duocuc.cl>
2025-05-02 19:48:38 -0400

    Add images to index and services
     Add the photos, it needs to be responsive

--- petti/settings.py
+++ petti/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'petti' / 'static']
 LOGIN_REDIRECT_URL = reverse_lazy('index')
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
